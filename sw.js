d78b8a98293648931e1d16865356d8ecd350854e
MrCsabaToth <csaba.toth.us@gmail.com>
2019-07-04 20:33:24 -0700

    Upgrading jQuery to 3, BS to 4.3.1, new BS color theme (based on bootstrap.build Minty), streamline many pieces

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'csaba.page',
-    suffix: 'v1',
+    suffix: 'v1.1',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
