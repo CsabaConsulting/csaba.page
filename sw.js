dca398486cd3a9440e3dd071b121dd79c214cea4
MrCsabaToth <csaba.toth.us@gmail.com>
2019-07-21 23:50:03 -0700

    Precache fonts

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'csaba.page',
-    suffix: 'v1.3',
+    suffix: 'v1.4',
     precache: 'precache',
     runtime: 'runtime-cache'
 });
